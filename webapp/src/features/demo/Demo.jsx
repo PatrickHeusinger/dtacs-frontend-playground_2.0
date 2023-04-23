03c4b500365953d5ea7b8dbdc18d7e2b6951e0a1
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2023-04-23 19:10:06 +0200

    add map function

--- webapp/src/features/demo/Demo.jsx
+++ webapp/src/features/demo/Demo.jsx
@@ -1,39 +1,47 @@
-import {Box, Checkbox, Group, Stack } from '@mantine/core';
-import { nanoid } from '@reduxjs/toolkit'
-import {useDispatch, useSelector} from "react-redux";
-import {setSelectedFilter} from "../../app/mainPageSlice";
+import { Box, Checkbox, Group, Stack } from '@mantine/core';
+import { nanoid } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedFilter } from '../../app/mainPageSlice';
 
-console.log(nanoid())
+console.log(nanoid());
 
 
 export default function Demo() {
-    const dispatch = useDispatch();
-    const { selectedFilter } = useSelector((state) => {
-        return { selectedFilter: state.MainPage.selectedFilter };
+  const dispatch = useDispatch();
+  const { selectedFilter } = useSelector((state) => {
+    return { selectedFilter: state.MainPage.selectedFilter };
 
-    });
-    console.log(selectedFilter)
+  });
+  console.log(selectedFilter);
+
+  const items = ['react', 'svelte', 'angular', 'vue', 'flask'];
+
   return (
-      <Box sx={{display:'flex', justifyContent:'center', height: '100%' }}>
-          <Stack sx={{display:'flex', justifyContent:'center',alignItems:'center'}}>
-              <Checkbox.Group
-                  value={selectedFilter}
-                  label="Select your favorite frameworks/libraries"
-                  description="This is anonymous"
-                  withAsterisk
-                  onChange={
-                      (event) => {dispatch(setSelectedFilter(event))}
-                  }
+    <Box sx={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+      <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Checkbox.Group
+          value={selectedFilter}
+          label='Select your favorite frameworks/libraries'
+          description='This is anonymous'
+          withAsterisk
+          onChange={
+            (event) => {
+              dispatch(setSelectedFilter(event));
+            }
+          }
 
-              >
-                  <Group mt="xs">
-                      <Checkbox value="react" label="React" />
-                      <Checkbox value="svelte" label="Svelte" />
-                      <Checkbox value="angular" label="Angular" />
-                      <Checkbox value="vue" label="Vue" />
-                  </Group>
-              </Checkbox.Group>
-          </Stack>
-      </Box>
+        >
+          <Group mt='xs'>
+            {
+              items.map((value) => (
+
+                <Checkbox key={nanoid()} value={value} label={value.toUpperCase()} />
+
+              ))
+            }
+          </Group>
+        </Checkbox.Group>
+      </Stack>
+    </Box>
   );
 }
